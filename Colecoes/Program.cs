f31624c7dacce3bafb1ca6cb4addf20b2fb11a10
Murilo Olegini <muriloolegini@outlook.com>
2022-06-24 14:56:52 -0300

    Array multidimensional

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -4,27 +4,45 @@
     {
         public static void Main(string[] args)
         {
-            //Criando e percorrendo um array
-
-            int[] arrayInteiros = new int[3];
-
-            arrayInteiros[0] = 10;
-            arrayInteiros[1] = 20;
-            arrayInteiros[2] = 30;
-
-            Console.WriteLine("Percorrendo array pelo for!");
-
-            for (int i = 0; i < arrayInteiros.Length; i++)
+            #region Matriz
+            int[,] matriz = new int[4, 2]
             {
-                Console.WriteLine("Array: " + arrayInteiros[i]);
-            }
+                { 5, 10 },
+                { 11, 20 },
+                { 21, 30 },
+                { 31, 50 }
+            };
 
-            Console.WriteLine("Percorrendo array pelo foreach!");
-
-            foreach (int item in arrayInteiros)
+            for (int i = 0; i < matriz.GetLength(0); i++)
             {
-                Console.WriteLine("Array: " + item);
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.WriteLine(matriz[i, j]);
+                }
             }
+            #endregion
+
+            #region Array
+            //int[] arrayInteiros = new int[3];
+
+            //arrayInteiros[0] = 10;
+            //arrayInteiros[1] = 20;
+            //arrayInteiros[2] = int.Parse("30");
+
+            //Console.WriteLine("Percorrendo array pelo for!");
+
+            //for (int i = 0; i < arrayInteiros.Length; i++)
+            //{
+            //    Console.WriteLine("Array: " + arrayInteiros[i]);
+            //}
+
+            //Console.WriteLine("Percorrendo array pelo foreach!");
+
+            //foreach (int item in arrayInteiros)
+            //{
+            //    Console.WriteLine("Array: " + item);
+            //}
+            #endregion
         }
     }
 }
