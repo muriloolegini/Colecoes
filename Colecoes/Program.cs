74f1dc009448fb153b5dc94b31762f2b5e510147
Murilo Olegini <muriloolegini@outlook.com>
2022-06-24 14:17:08 -0300

    Add project files.

---
+++ Colecoes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Colecoes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Criando e percorrendo um array
+
+            int[] arrayInteiros = new int[3];
+
+            arrayInteiros[0] = 10;
+            arrayInteiros[1] = 20;
+            arrayInteiros[2] = 30;
+
+            Console.WriteLine("Percorrendo array pelo for!");
+
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                Console.WriteLine("Array: " + arrayInteiros[i]);
+            }
+
+            Console.WriteLine("Percorrendo array pelo foreach!");
+
+            foreach (int item in arrayInteiros)
+            {
+                Console.WriteLine("Array: " + item);
+            }
+        }
+    }
+}
