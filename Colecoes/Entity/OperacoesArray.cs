4d8ac1230807a8fb442969cfb96acd95e895819e
Murilo Olegini <muriloolegini@outlook.com>
2022-06-25 16:46:50 -0300

    Ordenando array

---
+++ Colecoes/Entity/OperacoesArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecoes.Entity
+{
+    public class OperacoesArray
+    {
+        public void OrdenarBubbleSort(ref int[] array)
+        {
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+        public void ImprimirArray(int[] array)
+        {
+            var linha = string.Join(", ", array);
+            Console.WriteLine(linha);
+        }
+
+        public void Ordener(ref int[] array)
+        {
+            Array.Sort(array);
+        }
+
+        public void Copiar(ref int[] array, ref int[] arrayDestino)
+        {
+            Array.Copy(array, arrayDestino, array.Length);
+        }
+    }
+}
--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,25 +1,50 @@
-﻿namespace Colecoes
+﻿using Colecoes.Entity;
+
+namespace Colecoes
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            #region Matriz
-            int[,] matriz = new int[4, 2]
-            {
-                { 5, 10 },
-                { 11, 20 },
-                { 21, 30 },
-                { 31, 50 }
-            };
+            #region Ordenando Array
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] { 6, 3, 8, 1, 9 };
+            int[] arrayCopia = new int[10];
 
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    Console.WriteLine(matriz[i, j]);
-                }
-            }
+            Console.WriteLine("Array original");
+            op.ImprimirArray(array);
+
+         // op.OrdenarBubbleSort(ref array);
+            op.Ordener(ref array);
+
+            Console.WriteLine("Array ordenado");
+            op.ImprimirArray(array);
+
+            Console.WriteLine("Array antes da cópia");
+            op.ImprimirArray(arrayCopia);
+
+            Console.WriteLine("Array depois da cópia");
+            op.Copiar(ref array, ref arrayCopia);
+
+            op.ImprimirArray(arrayCopia);
+            #endregion
+
+            #region Matriz
+            //int[,] matriz = new int[4, 2]
+            //{
+            //    { 5, 10 },
+            //    { 11, 20 },
+            //    { 21, 30 },
+            //    { 31, 50 }
+            //};
+
+            //for (int i = 0; i < matriz.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < matriz.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(matriz[i, j]);
+            //    }
+            //}
             #endregion
 
             #region Array
